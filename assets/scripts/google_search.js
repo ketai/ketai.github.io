39fe8d11b9108d8c2f9d5020dc10c5f0261ff9d6
jaskiratr <randj063@newschool.edu>
2015-08-13 01:50:16 -0400

    Search Update

---
+++ assets/scripts/google_search.js
@@ -0,0 +1,21 @@
+// (function() {
+//     var cx = '000151488356480034415:pyho2umm0po';
+//     var gcse = document.createElement('script');
+//     gcse.type = 'text/javascript';
+//     gcse.async = true;
+//     gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+//         '//cse.google.com/cse.js?cx=' + cx;
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(gcse, s);
+// })();
+
+(function() {
+    var cx = '000151488356480034415:pyho2umm0po'; // Insert your own Custom Search engine ID here
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
