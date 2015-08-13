bd18cd1a9e2187df3a93efa0728eb64dea010cba
jaskiratr <randj063@newschool.edu>
2015-08-13 01:59:47 -0400

    search update

--- assets/scripts/google_search.js
+++ assets/scripts/google_search.js
@@ -1,21 +1,22 @@
-// (function() {
-//     var cx = '000151488356480034415:pyho2umm0po';
-//     var gcse = document.createElement('script');
-//     gcse.type = 'text/javascript';
-//     gcse.async = true;
-//     gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-//         '//cse.google.com/cse.js?cx=' + cx;
-//     var s = document.getElementsByTagName('script')[0];
-//     s.parentNode.insertBefore(gcse, s);
-// })();
 
-(function() {
-    var cx = '000151488356480034415:pyho2umm0po'; // Insert your own Custom Search engine ID here
+  (function() {
+    var cx = '000151488356480034415:ixgzbzp0hcq';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
-    gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
-        '//www.google.com/cse/cse.js?cx=' + cx;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
-})();
+  })();
+
+// (function() {
+//     var cx = '017643444788069204610:4gvhea_mvga'; // Insert your own Custom Search engine ID here
+//     var gcse = document.createElement('script');
+//     gcse.type = 'text/javascript';
+//     gcse.async = true;
+//     gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+//         '//www.google.com/cse/cse.js?cx=' + cx;
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(gcse, s);
+// })();
